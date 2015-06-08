e6ad03e50233501a5fa714abcf87c67c95798833
ceceyu <ceceyu@gmail.com>
2015-06-08 09:12:06 -0700

    Works pretty bad

--- facebook-photo/FeedViewController.swift
+++ facebook-photo/FeedViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning {
+    
+    var isPresenting: Bool = true
     
     var selectedImageView: UIImageView!
 
@@ -27,7 +29,7 @@ class FeedViewController: UIViewController {
     }
     
     @IBAction func onPhotoTao(sender: UITapGestureRecognizer) {
-        selectedImageView = sender.view as! UIImageView        
+        selectedImageView = sender.view as! UIImageView
         performSegueWithIdentifier("photoSegue", sender: sender)
     }
 
@@ -37,6 +39,57 @@ class FeedViewController: UIViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         var destinationController = segue.destinationViewController as! PhotoViewController
         destinationController.photo = selectedImageView.image
+        destinationController.modalPresentationStyle = UIModalPresentationStyle.Custom
+        destinationController.transitioningDelegate = self
+    }
+    
+    // MARK: - Delegate methods
+
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresenting = true
+        return self
+    }
+    
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresenting = false
+        return self
+    }
+    
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
+        // The value here should be the duration of the animations scheduled in the animationTransition method
+        return 1.0
+    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        println("animating transition")
+        var containerView = transitionContext.containerView()
+        var toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+        var fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
+        var prestigeImageView = UIImageView(image: selectedImageView.image)
+        
+        if (isPresenting) {
+            
+            containerView.addSubview(toViewController.view)
+            fromViewController.view.addSubview(prestigeImageView)
+            prestigeImageView.frame = selectedImageView.frame
+            toViewController.view.alpha = 0
+            
+            UIView.animateWithDuration(0.2, animations: { () -> Void in
+                toViewController.view.alpha = 1
+                prestigeImageView.frame = (toViewController as!PhotoViewController).photoImageView.frame
+                }) { (finished: Bool) -> Void in
+                    transitionContext.completeTransition(true)
+                    prestigeImageView.removeFromSuperview()
+            }
+        } else {
+            
+            UIView.animateWithDuration(1.0, animations: { () -> Void in
+                
+                }) { (finished: Bool) -> Void in
+                    transitionContext.completeTransition(true)
+                    fromViewController.view.removeFromSuperview()
+            }
+        }
     }
 
 }
--- facebook-photo/PhotoViewController.swift
+++ facebook-photo/PhotoViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
-class PhotoViewController: UIViewController {
+class PhotoViewController: UIViewController, UIScrollViewDelegate  {
 
     var photo: UIImage!
     
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var doneButton: UIImageView!
+    @IBOutlet weak var actionButtons: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.delegate = self
 
         photoImageView.image = photo
+        
+        scrollView.contentSize = CGSize(width: 320, height: 800)
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,9 +34,56 @@ class PhotoViewController: UIViewController {
     
     @IBAction func onDoneTap(sender: UITapGestureRecognizer) {
         dismissViewControllerAnimated(true, completion: { () -> Void in
-            
         })
     }
+    
+    
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        
+        let scrollTranslation = scrollView.panGestureRecognizer.translationInView(scrollView).y
+        
+        if scrollView.panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            if scrollTranslation < 100 {
+                doneButton.alpha = 1 - (scrollTranslation/100)
+                actionButtons.alpha = 1 - (scrollTranslation/100)
+                scrollView.backgroundColor = UIColor(white: 0, alpha: 1 - (scrollTranslation/200))
+            }
+            
+            if scrollTranslation > 100 {
+                doneButton.alpha = 0
+                actionButtons.alpha = 0
+            }
+        }
+        
+    }
+    
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        // When user begins to drag
+    }
+    
+    
+    func scrollViewDidEndDragging(scrollView: UIScrollView,
+        willDecelerate decelerate: Bool) {
+            
+        let scrollTranslation = scrollView.panGestureRecognizer.translationInView(scrollView).y
+        if scrollTranslation > 100 {
+            dismissViewControllerAnimated(true, completion: { () -> Void in
+            })
+        }
+        else {
+            //Just go back to default behavior..
+        }
+        
+        doneButton.alpha = 1
+        actionButtons.alpha = 1
+        scrollView.backgroundColor = UIColor(white: 0, alpha: 1)
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        // This method is called when the scrollview finally stops scrolling.
+    }
+
 
     /*
     // MARK: - Navigation
